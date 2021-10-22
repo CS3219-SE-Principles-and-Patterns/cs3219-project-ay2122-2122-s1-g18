db8b4019741b96b72a43758d994bb0e4ba68827e
Kai En Foo <kaien.foo@gmail.com>
2021-10-23 04:50:33 +0800

    Add logic for deployment to Google App Engine

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,9 @@
-export const SERVER_URI = 'http://localhost:8000'
-export const SOCKET_URI = 'http://localhost:4000'
+const PRODUCTION_SERVER = 'https://cs3219-project-ay2122-s1-g18.as.r.appspot.com'
+
+export const SERVER_URI = process.env.NODE_ENV === 'production'
+  ? PRODUCTION_SERVER
+  : 'http://localhost:8000'
+
+export const SOCKET_URI = process.env.NODE_ENV === 'production'
+  ? PRODUCTION_SERVER
+  : 'http://localhost:4000'
